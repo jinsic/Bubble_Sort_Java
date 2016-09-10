9d038d8b730afdccf58b50b5392d128a15859ddc
jinsic <dlwlstlrsla@naver.com>
2016-09-10 19:29:44 +0900

    +Binary Serach

--- B_Sort/src/B_Sort.java
+++ B_Sort/src/B_Sort.java
@@ -3,14 +3,25 @@ public class B_Sort {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+
 		int arr[] = {1,3,5,7,12,5,867,234,3476,4564,24};
-		
 		Sort(arr);
 		
+		
+		if(B_Search(arr,234)==1)
+			System.out.println(234 + "존재");
+		else
+			System.out.println(234 + "없음");
+		
+			
+/*
 		for(int i=0;i<arr.length;++i)
 			System.out.print(arr[i] + " ");
+			
+			
 		
 		
+*/
 		}
 	
 	
@@ -31,6 +42,24 @@ public class B_Sort {
 		      }
 		    }
 		
+	}
+	public static int B_Search(int arr[], int findX)
+	{		
+		int bottom = 0;
+		int top = arr.length;
+		
+		while(top >= bottom)
+		{
+			int mid = (top+bottom)/2;
+			if(arr[mid] == findX)
+				return 1;
+			else if(arr[mid] < findX)
+				bottom = mid + 1;
+			else
+				top = mid - 1;
+		}
+		
+		return 0;
 	}
 
 
